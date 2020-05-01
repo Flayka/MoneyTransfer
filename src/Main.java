dc9bb9602a87c55678a55aa259d0c26e351149cb
Alexandra <alexandra@Alexandras-MacBook-Air.local>
2020-05-01 17:51:53 +0300

    Create repository

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 147_483_648;
+                int totalBalance = balance + transfer;
+        System.out.println(totalBalance);
+    }
+}
